37372a9343b9dc617c6661bf8db534b52570cbde
Cheldaev Alexey <alexothers35@gmail.com>
2022-11-05 19:03:46 +0700

    Added hash Encryptor

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
 # Log encryption
+
+cryptologging - это библиотека для шифрования отдельных данных лога.
+
+
+## Пример использования
+```python
+import json
+import logging
+from datetime import datetime
+
+from cryptologging.algorithms.hash import MD5HashEncryptor
+from cryptologging.formatter import CryptoFormatter
+
+logger = logging.getLogger('example')
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()
+
+
+class MySuperFormatter(CryptoFormatter):
+    """Форматтер, используемый в моей команде."""
+
+    def format(self, record: logging.LogRecord):
+        """Форматирование записи лога."""
+        formatted_record = {
+            'timestamp': datetime.now().isoformat(),
+            'application': 'my_super_application',
+            'message': {
+                'content': {
+                    'data': self.encrypt(record.msg),
+                } if isinstance(record.msg, dict) else {
+                    'text': record.msg,
+                },
+            },
+            'system': 'backend',
+        }
+        return json.dumps(formatted_record, default=str)
+
+
+handler.setFormatter(
+    MySuperFormatter(
+        encryptor=MD5HashEncryptor(),
+        fields={
+            'secret_field', 'secret_field1', 'secret_field2',
+        },
+    ),
+)
+logger.addHandler(handler)
+
+logger.info('String')
+logger.info(1)
+logger.info(1.6)
+logger.info(b'hello')
+logger.info(None)
+logger.info([1, 1.5, 'hello', b'hello'])
+logger.info((1, 1.5, 'hello', b'hello'))
+logger.info({1, 1.5, 'hello', b'hello'})
+logger.info(
+    {
+        'field': 'value',
+        'int_field': 1,
+        'float_field': 3.14,
+        'none_field': None,
+        'secret_field': 'secret_value',
+        'nested_field': {
+            'secret_field': 'secret_value',
+        },
+        'list_field': [
+            'element1', 'element2',
+        ],
+        'tuple_field': (
+            't_elem1', 't_elem2',
+        ),
+        'list_of_secret_data': [
+            {
+                'secret_field': 'secret_value',
+                'not_secret_field': 'value',
+            },
+            {
+                'secret_field': 'secret_value',
+                'not_secret_field': 'value',
+            },
+            [
+                {
+                    'secret_field1': ['secret_value'],
+                },
+            ],
+            {
+                'secret_field': 1,
+            },
+            {
+                'secret_field': 3.14,
+            },
+            {
+                'secret_field': (1, 2),
+            },
+            {
+                'not_secret': {
+                    'not_secret': {
+                        'not_secret': {
+                            'not_secret': {
+                                'not_secret': {
+                                    'secret_field2': None,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        ],
+    },
+)
+```
--- exmaple.py
+++ exmaple.py
@@ -1,11 +1,106 @@
+import json
 import logging
-from cryptologging.hashing.formatter import HashFormatter
+from datetime import datetime
+
+from cryptologging.algorithms.hash import MD5HashEncryptor
+from cryptologging.formatter import CryptoFormatter
 
 logger = logging.getLogger('example')
 logger.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler()
-handler.setFormatter(HashFormatter('%(name)-12s: %(levelname)-8s %(message)s'))
+
+
+class MySuperFormatter(CryptoFormatter):
+    """Форматтер, используемый в моей команде."""
+
+    def format(self, record: logging.LogRecord):
+        """Форматирование записи лога."""
+        formatted_record = {
+            'timestamp': datetime.now().isoformat(),
+            'application': 'my_super_application',
+            'message': {
+                'content': {
+                    'data': self.encrypt(record.msg),
+                } if isinstance(record.msg, dict) else {
+                    'text': record.msg,
+                },
+            },
+            'system': 'backend',
+        }
+        return json.dumps(formatted_record, default=str)
+
+
+handler.setFormatter(
+    MySuperFormatter(
+        encryptor=MD5HashEncryptor(),
+        fields={
+            'secret_field', 'secret_field1', 'secret_field2',
+        },
+    ),
+)
 logger.addHandler(handler)
 
-logger.info('Test')
+logger.info('String')
+logger.info(1)
+logger.info(1.6)
+logger.info(b'hello')
+logger.info(None)
+logger.info([1, 1.5, 'hello', b'hello'])
+logger.info((1, 1.5, 'hello', b'hello'))
+logger.info({1, 1.5, 'hello', b'hello'})
+logger.info(
+    {
+        'field': 'value',
+        'int_field': 1,
+        'float_field': 3.14,
+        'none_field': None,
+        'secret_field': 'secret_value',
+        'nested_field': {
+            'secret_field': 'secret_value',
+        },
+        'list_field': [
+            'element1', 'element2',
+        ],
+        'tuple_field': (
+            't_elem1', 't_elem2',
+        ),
+        'list_of_secret_data': [
+            {
+                'secret_field': 'secret_value',
+                'not_secret_field': 'value',
+            },
+            {
+                'secret_field': 'secret_value',
+                'not_secret_field': 'value',
+            },
+            [
+                {
+                    'secret_field1': ['secret_value'],
+                },
+            ],
+            {
+                'secret_field': 1,
+            },
+            {
+                'secret_field': 3.14,
+            },
+            {
+                'secret_field': (1, 2),
+            },
+            {
+                'not_secret': {
+                    'not_secret': {
+                        'not_secret': {
+                            'not_secret': {
+                                'not_secret': {
+                                    'secret_field2': None,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        ],
+    },
+)
